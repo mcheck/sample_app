cbdaa3fc4a0bc97e90e5bef594fef829ed98fd0b
mcheck <mcheck@sologroup.com>
2011-02-20 22:26:40 -0600

    Change page ilnks to partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 # SampleApp::Application.routes.draw do
+  get "users/new"
+
 #   get "pages/home"
 # 
 #   get "pages/contact"
@@ -8,6 +10,8 @@
 #   get "pages/help"
 
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
